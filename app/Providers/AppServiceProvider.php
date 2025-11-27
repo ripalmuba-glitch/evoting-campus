3d3ac548344010d15a7e8d24b0094d27bfb5b539
Rivaldianto <ripalmuba@gmail.com>
2025-11-27 20:15:40 +0700

    Force HTTPS for assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- JANGAN LUPA INI
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa HTTPS jika aplikasi berjalan di Production (Zeabur)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
